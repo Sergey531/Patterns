f9eb2110fceafd28e30b1d2abaa18f7afcd42ded
Sergey531 <74931449+Sergey531@users.noreply.github.com>
2021-10-30 11:07:38 +0300

    Add files via upload

---
+++ Observer/WeatherStation.java
@@ -0,0 +1,19 @@
+package Observer;
+
+public class WeatherStation {
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+
+
+        CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
+        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+
+        System.out.println("Day 1:");
+        weatherData.setMeasurements(80, 65, 30.4f);
+        System.out.println("Day 2:");
+        weatherData.setMeasurements(82, 70, 29.2f);
+        System.out.println("Day 3:");
+        weatherData.setMeasurements(78, 90, 29.2f);
+    }
+}
---
+++ adapter/challenge/DroneAdapter.java
@@ -0,0 +1,20 @@
+package adapter.challenge;
+
+import adapter.Duck;
+
+public class DroneAdapter implements Duck {
+	Drone drone;
+ 
+	public DroneAdapter(Drone drone) {
+		this.drone = drone;
+	}
+    
+	public void quack() {
+		drone.beep();
+	}
+  
+	public void fly() {
+		drone.spin_rotors();
+		drone.take_off();
+	}
+}
